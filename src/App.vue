07edc53a358c59c9d0cb9d8b89611023388a0b16
Luis Suarez <luisdcobain@gmail.com>
2024-10-21 19:28:07 -0300

    Refactor App.vue and add Product and UserProfile components

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,26 @@
-<script setup>
+<script>
 import UserProfile from "./components/UserProfile.vue";
+import Product from "./components/Product.vue";
+
+export default {
+  name: "App",
+  components: {
+    UserProfile,
+    Product,
+  },
+};
 </script>
 
 <template>
-  <UserProfile />
+  <div class="container mt-5">
+    <h1 class="mb-4">Ejercicios Vue con Bootstrap</h1>
+
+    <h2 clas="mb-3">Ejercicio 1: Perfil de Usuario</h2>
+    <UserProfile />
+
+    <h2 clas="mb-3">Ejercicio 2: Productos</h2>
+    <Product />
+  </div>
 </template>
 
 <style scoped>
